1be6ee4862bee2a99bb8b3a05da8aa7fcd667a6c
indrajit <indrajeet654@gmail.com>
2021-02-07 11:03:21 +0530

    final commit

--- app/modules/mongoose.js
+++ app/modules/mongoose.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose')
 
 require('dotenv').config()
 
-mongoose.connect("mongodb+srv://pandey:aayu829@cluster0.n0hp2.mongodb.net/todos?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://pandey:aayu829@cluster0.n0hp2.mongodb.net/todos?retryWrites=true&w=majority");
 
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'mongodb error: '))
 
-db.once('open', () => console.log('mongodb connect'))
+db.once('open', () => console.log('mongodb connected'))
 
 module.exports = mongoose
